ae8fcf5b22edee9d77c7f94c8de19315b03f2cb0
Yonnw <2272866673@qq.com>
2020-10-18 18:38:37 +0800

    新增内容

--- src/MyText.java
+++ src/MyText.java
@@ -2,6 +2,7 @@ public class MyText {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("Hello Word GIT !");
+        System.out.println("我一点都不好！");
     }
 }
     
